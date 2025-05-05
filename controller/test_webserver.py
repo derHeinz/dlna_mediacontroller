3537f8856730ba1a4327962c175fd72eae988753
derHeinz <heinzserfas@gmx.de>
2025-05-05 23:05:18 +0200

    reactiving all tests

--- controller/test_webserver.py
+++ controller/test_webserver.py
@@ -45,7 +45,7 @@ class TestWebServer(unittest.TestCase):
             self.assertEqual(200, response.status_code)
             self.assertEqual(b"DLNA Media Controller", response.data)
 
-    def _test_info(self):
+    def test_info(self):
         client = self.client()
 
         self.APPINFO.get.assert_not_called()
@@ -53,7 +53,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.APPINFO.get.assert_called()
 
-    def _test_state(self):
+    def test_state(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.state.return_value = {'foo': 'bar'}
@@ -61,7 +61,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.DEFAULT_DISPATCHER.state.assert_called()
 
-    def _test_state_with_json(self):
+    def test_state_with_json(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.state.return_value = {'foo': 'bar'}
@@ -69,7 +69,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.DEFAULT_DISPATCHER.state.assert_called()
 
-    def _test_stop(self):
+    def test_stop(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.stop.return_value = {'foo': 'bar'}
@@ -77,7 +77,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.DEFAULT_DISPATCHER.stop.assert_called()
 
-    def _test_pause(self):
+    def test_pause(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.pause.return_value = {'foo': 'bar'}
@@ -85,7 +85,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.DEFAULT_DISPATCHER.pause.assert_called()
 
-    def _test_play_404(self):
+    def test_play_404(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.play.return_value = TestWebServer.MyState(None)
@@ -94,7 +94,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(404, response.status_code)
         self.DEFAULT_DISPATCHER.play.assert_called()
 
-    def _test_play_200(self):
+    def test_play_200(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.play.return_value = TestWebServer.MyState('foo')
@@ -103,7 +103,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.DEFAULT_DISPATCHER.play.assert_called()
 
-    def _test_play_request_invalid(self):
+    def test_play_request_invalid(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.play.side_effect = RequestInvalidException()
@@ -112,7 +112,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(400, response.status_code)
         self.DEFAULT_DISPATCHER.play.assert_called()
 
-    def _test_play_request_cannot_handeled(self):
+    def test_play_request_cannot_handeled(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.play.side_effect = RequestCannotBeHandeledException("foo")
@@ -121,7 +121,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(500, response.status_code)
         self.DEFAULT_DISPATCHER.play.assert_called()
 
-    def _test_play_error(self):
+    def test_play_error(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.play.side_effect = KeyError()
@@ -130,7 +130,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(500, response.status_code)
         self.DEFAULT_DISPATCHER.play.assert_called()
 
-    def _test_pause_error(self):
+    def test_pause_error(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.pause.side_effect = KeyError()
@@ -139,7 +139,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(500, response.status_code)
         self.DEFAULT_DISPATCHER.pause.assert_called()
 
-    def _test_pause_request_cannot_handeled(self):
+    def test_pause_request_cannot_handeled(self):
         client = self.client()
 
         self.DEFAULT_DISPATCHER.pause.side_effect = RequestCannotBeHandeledException("foo")
@@ -148,7 +148,7 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual(500, response.status_code)
         self.DEFAULT_DISPATCHER.pause.assert_called()
 
-    def _test_pause_cors(self):
+    def test_pause_cors(self):
         config = {'webserver_port': 17, 'webserver_cors_allow': True}
         webserver = self._create_webserver(config)
         client = self.client(webserver)
@@ -160,8 +160,8 @@ class TestWebServer(unittest.TestCase):
         self.assertEqual('*', response.headers.get('Access-Control-Allow-Origin'))
         self.DEFAULT_DISPATCHER.pause.assert_called()
 
-    def _test_exit(self):
-        webserver = self._testee()
+    def test_exit(self):
+        webserver = self._create_webserver()
         
         with patch.object(webserver, '_exit_program', wraps=webserver._exit_program) as wrapped_exit:
             wrapped_exit.return_value = None  # make it not a spy, thus method not called
